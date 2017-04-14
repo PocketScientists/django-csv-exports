9cead457ac732851173cdad21c4b55c82e748b47
Florian Demmer <fdemmer@gmail.com>
2017-04-14 14:58:11 +0200

    Replace testrunenr with pytest

--- runtests.py
+++ runtests.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python
+import os
 import sys
-
-from django.conf import settings
-
-
-if not settings.configured:
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:',
-            }
-        },
-        INSTALLED_APPS=(
-            'django_csv_exports',
-        ),
-        SITE_ID=1,
-        SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
-    )
+import pytest
 
 
-from django.test.utils import get_runner
-
-
-def runtests():
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
-    failures = test_runner.run_tests(['django_csv_exports', ])
-    sys.exit(failures)
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    sys.path.insert(0, "test_project")
+    return pytest.main()
 
 
 if __name__ == '__main__':
-    runtests()
-
+    sys.exit(main())
